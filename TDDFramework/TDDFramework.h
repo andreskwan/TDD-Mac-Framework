7e4118c1023db692d10381e9602ca66d48552832
Andres Kwan <andreskwan@gmail.com>
2021-08-26 16:58:14 -0500

    Initial Commit

---
+++ TDDFramework/TDDFramework.h
@@ -0,0 +1,18 @@
+//
+//  TDDFramework.h
+//  TDDFramework
+//
+//  Created by Andres Kwan on 26/08/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TDDFramework.
+FOUNDATION_EXPORT double TDDFrameworkVersionNumber;
+
+//! Project version string for TDDFramework.
+FOUNDATION_EXPORT const unsigned char TDDFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TDDFramework/PublicHeader.h>
+
+
