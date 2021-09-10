36ee4e12398d5d5513f00bdf581e4a5a4d452e53
Andres Kwan <andreskwan@gmail.com>
2021-09-10 03:43:15 -0500

    [Quotes] - remember tags cam be represented by an array or a dictionary

---
+++ TDDFramework/Quotes.swift
@@ -0,0 +1,122 @@
+// This file was generated from JSON Schema using quicktype, do not modify it directly.
+// To parse the JSON, add this file to your project and do:
+//
+//   let quotes = try? newJSONDecoder().decode(Quotes.self, from: jsonData)
+
+import Foundation
+
+// MARK: - Quotes
+struct Quotes: Codable {
+    let success: Success
+    let contents: Contents
+    let baseurl: String
+    let copyright: Copyright
+}
+
+// MARK: - Contents
+struct Contents: Codable {
+    let quotes: [Quote]
+}
+
+// MARK: - Quote
+struct Quote: Codable {
+    let length, quote, author: String
+    //Decoding the Quote was failing because tags can be an [Strings] or a dictionary!
+    let tags: [String: String]
+    let category, language, date: String
+    let permalink: String
+    let id: String
+    let background: String
+    let title: String
+}
+
+// MARK: - Copyright
+struct Copyright: Codable {
+    let year: Int
+    let url: String
+}
+
+// MARK: - Success
+struct Success: Codable {
+    let total: Int
+}
+
+let json404 = """
+{
+    "error": {
+        "code": 404,
+        "message": "Not Found"
+    }
+}
+"""
+
+let tagsAsDictionaries = """
+    {
+    "success": {
+        "total": 1
+    },
+    "contents": {
+        "quotes": [
+            {
+                "quote": "If you don't have confidence, you'll always find a way not to win",
+                "length": "65",
+                "author": "Carl Lewis",
+                "tags": {
+                    "0": "confidence",
+                    "1": "inspire",
+                    "3": "trust",
+                    "4": "winning"
+                },
+                "category": "inspire",
+                "language": "en",
+                "date": "2021-08-23",
+                "permalink": "https://theysaidso.com/quote/carl-lewis-if-you-dont-have-confidence-youll-always-find-a-way-not-to-win",
+                "id": "4sjGQiAAZhY6jCxr6XcV1QeF",
+                "background": "https://theysaidso.com/img/qod/qod-inspire.jpg",
+                "title": "Inspiring Quote of the day"
+            }
+        ]
+    },
+    "baseurl": "https://theysaidso.com",
+    "copyright": {
+        "year": 2023,
+        "url": "https://theysaidso.com"
+    }
+}
+"""
+
+
+let tagsAsArrays = """
+{
+    "success": {
+        "total": 1
+    },
+    "contents": {
+        "quotes": [
+            {
+                "quote": "Somewhere between the bottom of the climb and the summit is the answer to the mystery why we climb.",
+                "length": "99",
+                "author": "Greg Child",
+                "tags": [
+                    "inspire",
+                    "life",
+                    "meaning",
+                    "mountaineering"
+                ],
+                "category": "inspire",
+                "language": "en",
+                "date": "2021-08-24",
+                "permalink": "https://theysaidso.com/quote/greg-child-somewhere-between-the-bottom-of-the-climb-and-the-summit-is-the-answe",
+                "id": "4wWSyA_IO87jFwNPKkN7aweF",
+                "background": "https://theysaidso.com/img/qod/qod-inspire.jpg",
+                "title": "Inspiring Quote of the day"
+            }
+        ]
+    },
+    "baseurl": "https://theysaidso.com",
+    "copyright": {
+        "year": 2023,
+        "url": "https://theysaidso.com"
+    }
+}
+"""
---
+++ TDDFrameworkTests/Asynchronous tests/AsyncNetworkCallsTests.swift
@@ -0,0 +1,130 @@
+//
+//  AsyncNetworkCalls.swift
+//  TDD-Katas-MacOSTests
+//
+//  Created by Andres Kwan on 22/08/21.
+//
+//more api to test
+//read the post https://developer.apple.com/forums/thread/116124
+//https://docs.mhw-db.com
+//https://github.com/LartTyler/MHWDB-API
+//https://developers.themoviedb.org/3/getting-started/introduction
+
+import XCTest
+@testable import TDDFramework
+
+final class AsyncNetworkCallsTests: XCTestCase {
+    let timeout: TimeInterval = 2
+    var expectation: XCTestExpectation!
+    
+    override func setUp() {
+        expectation = expectation(description: "Server responds in reasonable time")
+    }
+    
+    func test_noServerResponse() {
+        let url = URL(string: "invalidURL")!
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            defer { self.expectation.fulfill() }
+            XCTAssertNil(data)
+            XCTAssertNil(response)
+            XCTAssertNotNil(error)
+        }
+        .resume()
+        
+        waitForExpectations(timeout: timeout)
+    }
+    
+    func test_decodeData() {
+        let url = getValidUrl()
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            defer { self.expectation.fulfill() }
+            
+            XCTAssertNil(error)
+            
+            do {
+                let response = try XCTUnwrap(response as? HTTPURLResponse)
+                XCTAssertEqual(response.statusCode, 200)
+                let data = try XCTUnwrap(data)
+                print(String(decoding: data, as: UTF8.self))
+                XCTAssertNoThrow(
+                    try JSONDecoder().decode(Quotes.self, from: data)
+                )
+            }
+            catch { }
+        }.resume()
+        
+        waitForExpectations(timeout: timeout)
+    }
+    
+    ///this test is not testing for 404 data
+    ///it is usefult to validate the errors while decoding data
+    func test_requestWithResponse404_shouldThrowError_ifDecodingAQuote() {
+        //given
+        let url = get404WithUrl()
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            defer { self.expectation.fulfill() }
+            
+            XCTAssertNil(error)
+            
+            do {
+                let response = try XCTUnwrap(response as? HTTPURLResponse)
+                XCTAssertEqual(response.statusCode, 404)
+                
+                let data = try XCTUnwrap(data)
+                print(String(decoding: data, as: UTF8.self))
+                
+                //it should throw error, because it returned JSON data representing the error not a quote
+                //it should decode an error not a quote
+                _ = try JSONDecoder().decode(Quotes.self, from: data)
+            }
+            catch {
+                switch error {
+                case DecodingError.typeMismatch:
+                    print("\(error)")
+                case DecodingError.dataCorrupted:
+                    print("\(error)")
+                case DecodingError.keyNotFound(let key, _):
+                    print("Error -------: \(error)")
+                    print("Key -------: \(key)")
+                default:
+                    print("default-Error -------: \(error)")
+                    XCTFail("\(error)")
+                }
+            }
+        }
+        .resume()
+        
+        waitForExpectations(timeout: timeout)
+    }
+    
+//    commented because the setUp() is setting the expectation for this test
+//    but that expectation is not being waited
+//    func test_apiClient() {
+//
+//    }
+    
+    //MARK: Helpers
+    fileprivate func getValidUrl() -> URL {
+        var components = URLComponents()
+        components.scheme = "http"
+        components.host = "quotes.rest"
+        components.path = "/qod.json"
+        
+        let queryItem = URLQueryItem(name: "category", value: "inspire")
+        components.queryItems = [queryItem]
+        return components.url!
+    }
+
+    fileprivate func get404WithUrl() -> URL {
+        var components = URLComponents()
+        components.scheme = "http"
+        components.host = "quotes.rest"
+        components.path = "/pod.json"
+        
+        let queryItem = URLQueryItem(name: "category", value: "inspire")
+        components.queryItems = [queryItem]
+        return components.url!
+    }
+}
