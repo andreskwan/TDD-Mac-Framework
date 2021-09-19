738af8f08968d992f6c4dd9cf851e59550dba8b4
Andres Kwan <andreskwan@gmail.com>
2021-09-19 12:35:37 -0500

    [Refactoring]-[ch1] - Adding models, function to refactor and json data

---
+++ TDDFramework/Chapter1/Plays.swift
@@ -0,0 +1,91 @@
+//
+//  Plays.swift
+//  TDDFramework
+//
+//  Created by Andres Kwan on 19/09/21.
+//
+
+import Foundation
+
+// This file was generated from JSON Schema using quicktype, do not modify it directly.
+// To parse the JSON, add this file to your project and do:
+//
+//   let invoices = try? newJSONDecoder().decode(Invoices.self, from: jsonData)
+
+import Foundation
+
+// MARK: - Invoices
+struct Invoices: Codable {
+    let invoice: [Invoice]
+}
+
+// MARK: - Invoice
+struct Invoice: Codable {
+    let customer: String
+    let performances: [Performance]
+}
+
+// MARK: - Performance
+struct Performance: Codable {
+    let playID: String
+    let audience: Int
+}
+
+// MARK: - Plays
+struct Plays: Codable {
+    let plays: [Play]
+}
+
+// MARK: - Play
+struct Play: Codable {
+    let playID, name, type: String
+}
+
+class Cost {
+    func statement(invoice: Invoice, plays: [Play]) -> String {
+        var totalAmount = 0
+        var volumeCredits = 0.0
+        var result = "Statement for \(invoice.customer)\n";
+        //          const format = new Intl.NumberFormat("en-US",
+        //                                { style: "currency", currency: "USD",
+        //                                  minimumFractionDigits: 2 }).format;
+        
+        for performance in invoice.performances {
+            let play = plays.first(where: { $0.playID == performance.playID})
+            var thisAmount = 0
+            
+            switch play?.type {
+            case "tragedy":
+                thisAmount = 40000
+                if (performance.audience > 30) {
+                    thisAmount += 1000 * (performance.audience - 30)
+                }
+                break
+            case "comedy":
+                thisAmount = 30000
+                if (performance.audience > 20) {
+                    thisAmount += 1000 * (performance.audience - 20)
+                }
+                break
+            default:
+                fatalError("Unknown type: \(String(describing: play?.type))")
+            }
+            
+            // add volume credits
+            volumeCredits += Double(max(performance.audience - 30, 0))
+            
+            // add extra credit for every ten comedy attendees
+            if ("comedy" == play?.type) {
+                let value = floor(Double(performance.audience) / 5)
+                volumeCredits += value
+            }
+            
+            // print line for this order
+            result += "  \(String(describing: play?.name)): \n"
+            totalAmount += thisAmount;
+        }
+        result += "Amount owed is ${format(totalAmount/100)}\n";
+        result += "You earned ${volumeCredits} credits\n";
+        return result;
+    }
+}
---
+++ TDDFrameworkTests/RefactoringBookTest/Chapter1/PlaysData.swift
@@ -0,0 +1,47 @@
+//
+//  PlaysData.swift
+//  TDDFrameworkTests
+//
+//  Created by Andres Kwan on 19/09/21.
+//
+
+import Foundation
+
+public let playsData = """
+{
+    "plays": [{
+        "playID": "hamlet",
+        "name": "Hamlet",
+        "type": "tragedy"
+    }, {
+        "playID": "as-like",
+        "name": "As You Like It",
+        "type": "comedy"
+    }, {
+        "playID": "othello",
+        "name": "Othello",
+        "type": "tragedy"
+    }]
+}
+""".data(using: .utf8)!
+
+let invoices = """
+{
+    "invoices": [{
+        "customer": "BigCo",
+        "performances": [{
+                "playID": "hamlet",
+                "audience": 55
+            },
+            {
+                "playID": "as-like",
+                "audience": 35
+            },
+            {
+                "playID": "othello",
+                "audience": 40
+            }
+        ]
+    }]
+}
+""".data(using: .utf8)!
