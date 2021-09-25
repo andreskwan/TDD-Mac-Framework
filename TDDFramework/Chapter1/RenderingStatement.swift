e9ff1959fc2465dddc0175f30d1320712f5275fb
Andres Kwan <andreskwan@gmail.com>
2021-09-25 15:38:38 -0500

    [Renaming] [to make clear the intent]

---
+++ TDDFramework/Chapter1/RenderingStatement.swift
@@ -0,0 +1,33 @@
+//
+//  Plays.swift
+//  TDDFramework
+//
+//  Created by Andres Kwan on 19/09/21.
+//
+
+import Foundation
+
+/*
+ Requested changes
+ 1 - statement printed in HTML
+ 2 - add more play types
+ */
+
+func statement(invoice: Invoice, plays: [Play]) -> String {
+    return renderPlainText(StatementData(invoice, plays))
+}
+
+func renderPlainText(_ statement: StatementData) -> String {
+    var result = "Statement for \(statement.customer)\n";
+    
+    for aPerformance in statement.performances {
+        guard let name = aPerformance.play?.name else { break }
+        result += "  \(name): \(usd(aPerformance.amount)) (\(aPerformance.audience) seats)\n"
+    }
+    
+    result += "Amount owed is \(usd(statement.totalAmount))\n";
+    result += "You earned \(Int(statement.totalVolumeCredits)) credits\n";
+    return result;
+}
+
+
