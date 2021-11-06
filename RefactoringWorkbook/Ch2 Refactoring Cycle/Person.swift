a0cf41c0e30a374ecdcdf38f3534c6c2dd5035ed
Andres Kwan <andreskwan@gmail.com>
2021-11-06 13:18:49 -0500

    [Encapsulate variable] - [Questions]

--- RefactoringWorkbook/Ch2 Refactoring Cycle/Person.swift
+++ RefactoringWorkbook/Ch2 Refactoring Cycle/Person.swift
@@ -8,10 +8,15 @@
 import Foundation
 
 //[Workbook] [ch2] - Encapsulate Variable - Refactoring
+/// questions?
+/// this prevents the smell global data?
+/// is this an smell
+/// how to make it evident?
+/// is there an example of replacing a variable with a function?
 class Person {
     private(set) var name: String
     
     init(name:String) {
-        self.name = name 
+        self.name = name
     }
 }
